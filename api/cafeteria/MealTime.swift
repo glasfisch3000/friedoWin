8cd6197b42a2d97c69ca087a2d999e80e2587dcc
glasfisch3000 <jakob@danckwerts.net>
2024-04-11 16:25:16 +0200

    added protocol conformances to Meal.Time

--- api/cafeteria/MealTime.swift
+++ api/cafeteria/MealTime.swift
@@ -15,3 +15,35 @@ extension Meal {
         case evening
     }
 }
+
+extension Meal.Time: Hashable { }
+
+extension Meal.Time: Identifiable {
+    var id: Self { self }
+}
+
+extension Meal.Time: Comparable {
+    var sortRank: Int {
+        switch self {
+        case .morning: 1
+        case .noon: 2
+        case .afternoon: 3
+        case .evening: 4
+        }
+    }
+    
+    static func < (lhs: Meal.Time, rhs: Meal.Time) -> Bool {
+        lhs.sortRank < rhs.sortRank
+    }
+}
+
+extension Meal.Time: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .morning: "Breakfast"
+        case .noon: "Lunch"
+        case .afternoon: "Afternoon"
+        case .evening: "Dinner"
+        }
+    }
+}
