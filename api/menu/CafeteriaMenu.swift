80fa8c737144634f568d37411a3d30eca0b43f8d
glasfisch3000 <jakob@danckwerts.net>
2024-04-11 16:36:41 +0200

    added FoodMenu type

---
+++ api/menu/CafeteriaMenu.swift
@@ -0,0 +1,12 @@
+//
+//  CafeteriaMenu.swift
+//  friedoWin
+//
+//  Created by Jakob Danckwerts on 10.04.24.
+//
+
+import Foundation
+
+extension Cafeteria {
+    typealias Menu = [Cafeteria: [Dish]]
+}
---
+++ api/menu/FoodMenu.swift
@@ -0,0 +1,10 @@
+//
+//  FoodMenu.swift
+//  friedoWin
+//
+//  Created by Jakob Danckwerts on 10.04.24.
+//
+
+import Foundation
+
+typealias FoodMenu = [Meal.Time: Cafeteria.Menu]
